08bdf28182c4a7e42a681abefa817578e10ac368
Nandana-PS-2003 <pnandana264@gmail.com>
2025-10-11 11:14:06 +0530

    11/10

--- billing software/salon_billing/settings.py
+++ billing software/salon_billing/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # or wherever your static folder is
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
